23eb9d8281ed471637d7c8a514b60f1bc917d096
ahad-s <ahad_cool@hotmail.co.uk>
2016-05-15 01:05:29 -0400

    ANN completed and debugged, made configurations easier to use

--- ann.py
+++ ann.py
@@ -3,65 +3,14 @@ import timeit
 import numpy as np
 from struct import unpack
 from scipy.optimize import fmin_cg
-
-from pymongo import MongoClient
-from bson.binary import Binary
 import pickle
 
-# mName = matrix of Name, p x q
+"""
+Created by: Ahad Shoaib
+"""
 
+ZERO_EPSILON = 0.0000000000000000000000000000000000000000000000000000000000000000000000000001
 
-# NOT NEEDED?
-class MongoDatabase(object):
-	
-	def __init__(self, new = False, X = None, y = None, theta1 = None, theta2 = None):
-
-
-
-		self._client = MongoClient("localhost", 27017)
-		self.db = self._client['digits-nn-database']
-
-		curr_set_number = retrieve_from_db()
-
-		if new:
-			curr_set_number = curr_set_number + 1
-			# make new entry with values of X, y, and theta1/2 
-
-
-		coll = db.test_collection # TODO Learn
-		obj = None
-		bytes = pickle.dump(obj)
-		coll.insert({'random-data': Binary(bytes)})
-
-
-	def update_X(X):
-		pass
-
-	def update_y():
-		pass
-
-	def update_theta1():
-		pass
-
-	def update_theta2():
-		pass
-
-	# retrieve from DB
-	def get_X():
-		pass
-
-	def get_y():
-		pass
-
-	def get_theta1():
-		pass
-
-	def get_theta2():
-		pass
-
-	def save():
-		# save all fields, i.e. create a new "finalized" entry
-		pass
 
 TRAINING_IMAGES_NAME = "train-images-idx3-ubyte"
 TRAINING_LABELS_NAME = "train-labels-idx1-ubyte"
@@ -78,8 +27,11 @@ class DataProcess(object):
 	global TEST_IMAGES_NAME
 	global TEST_LABELS_NAME
 
-	def __init__(self, training):
+	global ZERO_EPSILON
+
+	def __init__(self, training, m):
 		self.training = training
+		self.examples = m
 		self.X = None
 		self.y = None
 
@@ -103,9 +55,10 @@ class DataProcess(object):
 		labels_set.read(4)
 
 		# int
-		# num_images = images_set.read(4) # reduce this number for testing
-		# num_images = unpack('>I', num_images)[0] # >I == big endian unsigned int
-		num_images = 5
+		num_images = images_set.read(4) # reduce this number for testing
+		num_images = unpack('>I', num_images)[0] # >I == big endian unsigned int
+
+		num_images = self.examples
 
 		num_rows = images_set.read(4)
 		num_rows = unpack('>I', num_rows)[0]
@@ -115,34 +68,47 @@ class DataProcess(object):
 
 		# int
 		num_labels = unpack('>I', labels_set.read(4))[0]
-		print num_rows, num_cols, num_labels
+		# print num_rows, num_cols, num_labels
 
 		pixel_features = num_rows * num_cols
 
 		X_shape = (num_images, pixel_features)
 		y_shape = (num_images, 1)
 
-		X = np.empty(X_shape, dtype = np.int32)
-		y = np.empty(y_shape, dtype = np.int32)
+		X = np.empty(X_shape, dtype=np.float64)
+		y = np.empty(y_shape, dtype=np.float64)
 
 		for m in range(num_images): # training examples
-			print "Adding example [%d]" % m
+			# print "Adding example [%d]" % (m + 1)
 			for x_i in range(pixel_features): # features
 				X[m, x_i] = unpack('>B', images_set.read(1))[0]
 			y[m] = unpack('>B', labels_set.read(1))[0]
 
-		self.X = X;
+		if self.training:
+			print "added training examples..."
+		else:
+			print "added test examples..."
+
+		self.X = self.normalize_features(X);
 		self.y = y;
 
 		return (X, y)
 
-	def serialize_arr(val):
-		pass
+	def normalize_features(self, X):
+		min_arr = np.amin(X, axis=0)
+		max_arr = np.amax(X, axis=0)
 
+		norm_arr = np.linalg.norm(X, axis=0)
+		m = X.shape[0]
+		for i in xrange(m):
+			X[i, :] = X[i, :] / (norm_arr + ZERO_EPSILON)
+		print "normalization complete..."
 
+		return X
+
+	# ??? 
 	def visualize(self):
 		pass
-
 
 
 
@@ -152,23 +118,25 @@ class DataProcess(object):
 class ANN(object):
 
 	"""
-	ANN:
-	1. randomly init thetas
-	2. implement FP to get h_theta(x^i) for any x^i
-	3. implement code to compute J(theta) [formula]
-	4. implement BP to compute d(J(theta))/d-theta [partial derivative]
-	5. use gradient checking to compare grad using BP with gradApprox
-	6. use grad desc or "advanced optimization" to min theta J
+	PSEUDOCODE:
+	- randomly init thetas
+	- FP to get h_theta(x^i)
+	- compute J(theta)
+	- BP to compute d(J(theta))/d-theta [partial derivative]
+	- optimization to min theta J
+
+	TESTING: compare gradient/cost computed by BP/FP with grad desc
 	"""
+
+	global ZERO_EPSILON
 
 	INPUT_NEURONS = 784 # 28 x 28 image sizes
 	HIDDEN_NEURONS = 100 # may change later
 	OUTPUT_NEURONS = 10
 
-	M = 100 # training examples
-	TESTING_EXAMPLES = 0
-
-	LAMBDA = 1
+	# INPUT_NEURONS = 2
+	# HIDDEN_NEURONS = 2
+	# OUTPUT_NEURONS = 4
 
 	"""
 	theta1 = HIDDEN_NEURONS  x (INPUT_NEURONS + 1)
@@ -177,30 +145,41 @@ class ANN(object):
 	y = M x 1
 	"""
 
-	def __init__(self, theta1 = None, theta2 = None, X = None, y = None):
+	def __init__(self, theta = None, X = None, y = None, LAMBDA = 0.00001):
 		# TODO Load from theano/numpy/something as an array
-		self.X = "" # 6000 x (784 + 1)
-		self.y = "" # 6000 x 1
+		self.X = X # 6000 x (784 + 1)
+		self.y = y # 6000 x 1
+		self.M = X.shape[0]
+		self.LAMBDA = LAMBDA
 
-		if theta1 == None and theta2 == None:
-			theta1, theta2 = random_init(theta1, theta2)
-
-		# this is an array
-		self.theta = np.append(np.asarray(theta1.flatten()), 
+		if theta is None: # == will be elementwise check
+			theta1, theta2 = self.random_init()
+			# this is an array
+			self.theta = np.append(np.asarray(theta1.flatten()), 
 								np.asarray(theta2.flatten()))
-
-		self.theta1_total = HIDDEN_NEURONS * (INPUT_NEURONS + 1)
-		self.theta2_total = OUTPUT_NEURONS * (HIDDEN_NEURONS + 1)
-
-
-		# ma.item((x, y)) or ma.item(x)
-		# 
+		else:
+			self.theta = theta
 
 
-	def random_init(self, theta1, theta2):
+
+		self.theta1_total = self.HIDDEN_NEURONS * (self.INPUT_NEURONS + 1)
+		self.theta2_total = self.OUTPUT_NEURONS * (self.HIDDEN_NEURONS + 1)
+
+		# print ("totals 1. [%d] 2. [%d]" % (self.theta1_total, self.theta2_total))
+		# print ("t1 [%s] t2 [%s]" % (theta1.shape, theta2.shape))
+
+
+	def get_theta_mat(self):
+		return np.asmatrix(self.theta)
+
+	# default
+	def get_theta(self):
+		return np.asarray(self.theta)
+
+	def random_init(self):
 		# TODO: better random init?
-		theta1 = np.random.randn(HIDDEN_NEURONS, INPUT_NEURONS + 1)
-		theta2 = np.random.randn(OUTPUT_NEURONS, HIDDEN_NEURONS + 1)
+		theta1 = np.random.randn(self.HIDDEN_NEURONS, self.INPUT_NEURONS + 1)
+		theta2 = np.random.randn(self.OUTPUT_NEURONS, self.HIDDEN_NEURONS + 1)
 		return (theta1, theta2)
 
 
@@ -211,31 +190,36 @@ class ANN(object):
 
 	# z = 0 --> 0.25, large neg/pos z --> ~0
 	def sigmoid_gradient_fn(self, z):
-		return sigmoid_fn(z) * (1 - sigmoid_fn(z))
+		return np.multiply(self.sigmoid_fn(z), (1 - self.sigmoid_fn(z)))
 
 
 	# theta1, theta2 should be randomly init before
-	def cost(self, theta, X, y):
-		theta1, theta2 = unravel(theta)
+	def cost(self, theta, X, y, reg_const):
+		theta1, theta2 = self.unravel_theta(theta)
+
+		m = self.M
 
 		# cost value
 		J = 0
 
 		# add bias column to X
-		bias_col = np.ones((M, 1))
+		bias_col = np.ones((m, 1), dtype=np.float64)
 		X = np.append(bias_col, X, axis=1)
 
-		for i in range(M):
-
+		for i in range(m):
 			# forward propogation
 			#######################################################################
 
+			# print "M, theta1, X[i], bias, X"
+			# print self.M, theta1.shape, np.asmatrix(X[i]).shape, bias_col.shape, X.shape, type(X)
+
 			z_2 = np.dot(theta1, np.transpose(X[i]))
-			a_2 = sigmoid_fn(z = z_2) # HIDDEN_NEURONS x 1
-			a_2 = np.append(np.ones(1), a_2, axis = 1) # (HIDDEN_NEURONS + 1) x 1
+
+			a_2 = self.sigmoid_fn(z = z_2) # HIDDEN_NEURONS x 1
+			a_2 = np.vstack([np.asmatrix([1.0]), a_2]) # (HIDDEN_NEURONS + 1) x 1
 
 			z_3 = np.dot(theta2, a_2)
-			a_3 = sigmoid_fn(z = z_3) 
+			a_3 = self.sigmoid_fn(z = z_3) 
 
 			h_theta = a_3 # should be OUTPUT_NEURONS x 1
 
@@ -243,44 +227,63 @@ class ANN(object):
 			#######################################################################
 
 			# array of [OUTPUT_NEURONS] columns, 1 if y[i], 0 else
-			y_ik = np.asmatrix(np.arrange(OUTPUT_NEURONS)).transpose() == y[i]
+			y_ik = np.asmatrix(np.arange(self.OUTPUT_NEURONS), dtype=np.float64).transpose() == y[i]
 
-			J += np.sum((-y_ik) * log(h_theta) - (1 - y_ik) * log(1 - h_theta))
+			# fun python note: -y_ik converts all False->True, all True->False..
+			# I spent a whole cup of coffee figuring that out
+			# TODO: make the zero_epsilon solution nicer
+			J += np.sum(np.multiply((-1.0 * y_ik), np.log(h_theta + ZERO_EPSILON)) 
+				- np.multiply((1.0 - y_ik), np.log(1 - (h_theta + ZERO_EPSILON))))
+		# print "Before REG: J is [%f]" % J
 
-		# TODO: regularization
+		### REGULARIZATION ###
+
+		regsum = 0
+		# theta1 sum
+		t1_rows = theta1.shape[0]
+		t2_rows = theta2.shape[0]
+
+		for i in xrange(t1_rows):
+			regsum += np.sum(np.square(theta1[i, 2:]))
+
+		for i in xrange(t2_rows):
+			regsum += np.sum(np.square(theta2[i, 2:]))
+
+		regsum *= (reg_const / (2.0 * m))
+
+		J += regsum
+		# print "After REG: J is [%f]" % J
 
 		return J
 
 
-			"""
-	theta1 = (HIDDEN_NEURONS + 1) x (INPUT_NEURONS + 1)
-	theta2 = OUTPUT_NEURONS x (HIDDEN_NEURONS + 1)
-	X = M x (HIDDEN_NEURONS + 1)
-	y = M x 1
-	"""
+	def grad(self, theta, X, y, reg_const):
 
-	def grad(self, theta, X, y):
-		theta1, theta2 = unravel(theta)
+		theta1, theta2 = self.unravel_theta(theta)
+
+		m = self.M
 
 		# add bias column to X
-		bias_col = np.ones((M, 1))
+		bias_col = np.ones((m, 1), dtype=np.float64)
 		X = np.append(bias_col, X, axis=1)
 
 		# init for BP
 		Delta1 = np.zeros(theta1.shape)
-		Delta1 = np.zeros(theta2.shape)
-		delta3 = np.zeros(OUTPUT_NEURONS, 1)
+		Delta2 = np.zeros(theta2.shape)
+		delta3 = None
 
-		for i in range(M):
+		for i in range(m):
 			# forward propogation
 			#######################################################################
 
+			a_1 = X[i, :]
 			z_2 = np.dot(theta1, np.transpose(X[i]))
-			a_2 = sigmoid_fn(z = z_2) # HIDDEN_NEURONS x 1
-			a_2 = np.append(np.ones(1), a_2, axis = 1) # (HIDDEN_NEURONS + 1) x 1
+
+			a_2 = self.sigmoid_fn(z = z_2) # HIDDEN_NEURONS x 1
+			a_2 = np.vstack([np.asmatrix([1.0]), a_2]) # (HIDDEN_NEURONS + 1) x 1
 
 			z_3 = np.dot(theta2, a_2)
-			a_3 = sigmoid_fn(z = z_3) 
+			a_3 = self.sigmoid_fn(z = z_3) 
 
 			h_theta = a_3 # should be OUTPUT_NEURONS x 1
 
@@ -288,19 +291,20 @@ class ANN(object):
 			#######################################################################
 
 			# array of [OUTPUT_NEURONS] columns, 1 if y[i], 0 else
-			y_ik = np.asmatrix(np.arrange(OUTPUT_NEURONS)).transpose() == y[i]
+			y_ik = np.asmatrix(np.arange(self.OUTPUT_NEURONS)).transpose() == y[i]
 
 			# i.e. [0 1 2 3 ... 10] --> [0 0 1 0 ... 0] if y[i] = 3
 			delta3 = a_3 - y_ik
 
-			grad_sig = np.append(np.asmatrix(np.ones(1)), sigmoid_gradient_fn(z_2), axis=1)
-			delta2 = np.dot(np.transpose(theta2), delta3) * grad_sig
+			grad_sig = np.vstack([np.matrix([1]), self.sigmoid_gradient_fn(z_2)])
+			delta2 = np.multiply(np.dot(np.transpose(theta2), delta3), grad_sig)
 			delta2 = np.delete(delta2, 1, axis=0) # remove first row of delta2, corresponds to bias unit
 
 			Delta1 = Delta1 + np.dot(delta2, a_1)
-			Delta2 = Delta2 + np.dot(delta3, a_2)
+			Delta2 = Delta2 + np.dot(delta3, np.transpose(a_2))
 
-		# TODO REGULARIZATION FOR J
+
+		### REGULARIZATION ###
 
 		theta1_reg = theta1.copy()
 		theta1_reg[0] = 0
@@ -308,67 +312,95 @@ class ANN(object):
 		theta2_reg = theta2.copy()
 		theta2_reg[0] = 0
 
-		theta1_grad_reg = (LAMBDA / m) * theta1_reg
-		theta2_grad_reg = (LAMBDA / m) * theta2_reg
+		theta1_grad_reg = (float(reg_const) / m) * theta1_reg
+		theta2_grad_reg = (float(reg_const) / m) * theta2_reg
 
-		theta1_grad = Delta1 * (1 / m) + theta1_grad_reg
-		theta2_grad = Delta2 * (1 / m) + theta2_grad_reg
+		theta1_grad = Delta1 * (1.0 / m) + theta1_grad_reg
+		theta2_grad = Delta2 * (1.0 / m) + theta2_grad_reg
+
+		# theta1_grad = Delta1 * (1.0 / m)
+		# theta2_grad = Delta2 * (1.0 / m)
 
 
-		gradient1 = theta1_grad.flatten() # .shape = theta1.shape
-		gradient2 = theta2_grad.flatten() # .shape = theta2.shape
-		gradient = np.append(gradient1, gradient2, axis=0)
+		gradient1 = np.asarray(theta1_grad).ravel() # .shape = theta1.shape
+		gradient2 = np.asarray(theta2_grad).ravel() # .shape = theta2.shape
+		gradient = np.hstack([gradient1, gradient2]) # returns 1xsize ARRAY (should not be MATRIX because np.dot doesnt work)
 
-		return gradient
+		return gradient.transpose() # .transpose() allows fmin_cg to optimize properly
 
 	# requires trained theta1 and theta2
-	def predict(self, theta1, theta2, X_test):
-		print "Predicting..."
+	def predict(self, theta, X_test):
+		theta1, theta2 = self.unravel_theta(theta)
+		print "predicting..."
 
 		num_test = X_test.shape[0]
 
 		# bias unit
 		X_test = np.append(np.ones((num_test, 1)), X_test, axis=1)
 
-		a_2 = sigmoid_fn(np.dot(theta1, np.transpose(X_test)))
-		a_3 = sigmoid_fn(np.dot(theta2, a_2))
+		a_2 = self.sigmoid_fn(np.dot(theta1, np.transpose(X_test)))
+		a_2 = np.vstack([np.ones((1, X_test.shape[0])), a_2])
+		a_3 = self.sigmoid_fn(np.dot(theta2, a_2))
 
-		values, prediction = argmax(a_3, [], axis=1)
+		prediction = np.argmax(a_3, axis=0)
 
 		return np.transpose(prediction)
 
 	def unravel_theta(self, theta):
-		theta1 = np.unravel(np.take(theta, np.arange(self.theta1_total)), 
-				(HIDDEN_NEURONS, INPUT_NEURONS + 1))
-		theta2 = np.unravel(np.take(theta, np.arange(self.theta1_total, self.theta2_total)),
-				(OUTPUT_NEURONS, HIDDEN_NEURONS + 1))
+
+		theta1 = np.asmatrix(np.reshape(np.take(theta, np.arange(self.theta1_total)), (self.HIDDEN_NEURONS, self.INPUT_NEURONS + 1)))
+		theta2 = np.asmatrix(np.reshape(np.take(theta, np.arange(self.theta1_total, self.theta1_total + self.theta2_total)),
+				(self.OUTPUT_NEURONS, self.HIDDEN_NEURONS + 1)))
 		return (theta1, theta2)
 
-	# can use gradient descent, but this is faster
-	def train(self, myiter = 50):
-		print "Training..."
-		theta = fmin_cg(cost, self.theta, fprime = grad, maxiter = myiter)
-		return unravel(theta)
+
+	# will use SGD later maybe, this is temporary/faster
+	def train(self, myiter = 100):
+		print "training..."
+		init_theta = self.get_theta()
+		theta = fmin_cg(self.cost, init_theta, args = (self.X, self.y, self.LAMBDA), 
+			fprime = self.grad, maxiter = myiter)
+
+		return theta
 		
 
 
 if __name__ == '__main__':
-	train_data = DataProcess(training = True)
-	# test_data = DataProcess(training = False)
-	# database = MongoDatabase()
 
-	(X, y) = train_data.get_data()
+	# True to load from files, False to train ANN freshly
+	LOAD_FROM_FILE = False # WARNING: ANN TAKES A WHILE TO TRAIN!
 
-	neural_net = ANN(X=X, y=y)
+	TRAINING_EXAMPLES = 200
+	TESTING_EXAMPLES = 50
+	LAMBDA = 1
 
+	print "--------------"
 
-	print X
-	print y
-	# prediction = None
-	# print "Accuracy = [%.2f]" % ((y[np.where(prediction == y)].size / float(y.size)) * 100.00)
+	print "[TRAINING_EXAMLES = %d]"
 
+	train_data = DataProcess(training = True, m = TRAINING_EXAMPLES)
+	X, y = train_data.get_data()
 
+	test_data = DataProcess(training = False, m = TESTING_EXAMPLES)
+	X_test, y_test = test_data.get_data()
 
+	print "--------------"
 
-	# print X
-	# print y
+	if LOAD_FROM_FILE:
+		trained_theta = np.load("trained_params.npy", allow_pickle = True)
+		ann = ANN(theta = trained_theta, X = np.asmatrix(X), y = np.asmatrix(y))
+	else:
+		ann = ANN(X = np.asmatrix(X), y = np.asmatrix(y))
+		trained_theta = ann.train()
+		np.save("trained_params", trained_theta, allow_pickle = True)
+
+	print "--------------"
+
+	prediction = ann.predict(trained_theta, X_test)
+	print "[PREDICTIONS]:"
+	print prediction
+	print "[ACTUAL]:"
+	print y_test
+	print "Accuracy = [%.2f]" % ((y_test[np.where(prediction == y_test)].size / float(y_test.size)) * 100.00)
+
+	print "--------------"
